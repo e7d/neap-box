f57a239ca818d0b7fa0910fe81702b9fcd57e684
e7d <mail@e7d.io>
2015-12-22 17:26:06 +0100

    mute certificate generation

--- bootstrap/generate-certificates.sh
+++ bootstrap/generate-certificates.sh
@@ -7,7 +7,7 @@ try
     echo "Generate self-signed certificate"
     if [ ! -f /etc/ssl/localcerts/self.pem ]; then
         mkdir -p /etc/ssl/localcerts
-        openssl req -new -x509 -subj "/CN=`uname -n`/O=Neap/C=US" -days 3650 -nodes -out /etc/ssl/localcerts/self.pem -keyout /etc/ssl/localcerts/self.key
+        openssl req -new -x509 -subj "/CN=`uname -n`/O=Neap/C=US" -days 3650 -nodes -out /etc/ssl/localcerts/self.pem -keyout /etc/ssl/localcerts/self.key > /dev/null
         chmod -cR 600 /etc/ssl/localcerts/self.*
     else
         echo "skipped..."
