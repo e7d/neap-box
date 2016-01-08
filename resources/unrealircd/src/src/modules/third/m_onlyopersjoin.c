05fb5fdadfd1f39835e902576f4584099963d23f
e7d <mail@e7d.io>
2016-01-08 14:06:40 +0100

    fixed copyright

--- resources/unrealircd/src/src/modules/third/m_onlyopersjoin.c
+++ resources/unrealircd/src/src/modules/third/m_onlyopersjoin.c
@@ -1,7 +1,7 @@
 /**
  * Neap (http://neap.io/)
  *
- * @link      http://github.com/e7d/neap for the canonical source repository
+ * @link      http://github.com/e7d/neap-box for the canonical source repository
  * @copyright Copyright (c) 2015 Michaël "e7d" Ferrand (http://github.com/e7d)
  * @license   https://github.com/e7d/neap/blob/master/LICENSE.md The MIT License
  */
