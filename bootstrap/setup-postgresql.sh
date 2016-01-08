3efd336d0da23cfdbb2a81e814bbd2cae8066a28
e7d <mail@e7d.io>
2016-01-08 14:08:46 +0100

    updated PostgreSQL to 9.5

--- bootstrap/setup-postgresql.sh
+++ bootstrap/setup-postgresql.sh
@@ -4,8 +4,13 @@ try
 (
     throwErrors
 
+    echo "Add repository to Aptitude"
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main" >/etc/apt/sources.list.d/pgdg.list
+    apt-get -y -q update
+
     echo "Install packages"
-    apt-get -y -q install postgresql postgresql-contrib
+    apt-get -y -q install postgresql-9.5 postgresql-client-9.5 postgresql-client-common postgresql-common postgresql-contrib-9.5
 )
 catch || {
     case $ex_code in
