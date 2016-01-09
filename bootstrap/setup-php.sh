09cfbf95583bb84658cf716f66ed6289320286b9
e7d <mail@e7d.io>
2016-01-09 11:13:01 +0100

    added php7.0-sqlite3 extension

--- bootstrap/setup-php.sh
+++ bootstrap/setup-php.sh
@@ -12,7 +12,7 @@ try
 	apt-get -y -q update
 
 	echo "Install packages"
-	apt-get -y -q install php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-intl php7.0-json php7.0-opcache php7.0-pgsql
+	apt-get -y -q install php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-dev php7.0-fpm php7.0-gd php7.0-intl php7.0-json php7.0-opcache php7.0-pgsql php7.0-sqlite3
 
 	echo "Disable OPcache"
 	sed -i '/;opcache.enable=0/c\opcache.enable=0' /etc/php/7.0/cli/php.ini
