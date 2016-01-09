4890cf6953cc32d190aba982b1801ff1d777a6a6
e7d <mail@e7d.io>
2016-01-09 13:00:51 +0100

    updated compression script

--- bootstrap/zerodisk.sh
+++ bootstrap/zerodisk.sh
@@ -10,6 +10,8 @@ dd if=/dev/zero of="$swappart" bs=1M || echo "dd exit code $? is suppressed"
 echo "Nullify system free space"
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
 
 # Sync to ensure that the delete completes before this moves on.
 sync
