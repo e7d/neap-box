599f09eceb3ad431fb7f0cb3d8b23ec284625206
e7d <mail@e7d.io>
2016-02-16 17:58:50 +0100

    updated nginx build script to use version 1.9.11

--- bootstrap/build-nginx.sh
+++ bootstrap/build-nginx.sh
@@ -3,7 +3,7 @@
 . /vagrant/resources/colors.sh
 . /vagrant/resources/trycatch.sh
 
-NGINX_VERSION=1.9.10 # http://nginx.org/en/download.html
+NGINX_VERSION=1.9.11 # http://nginx.org/en/download.html
 OPENSSL_VERSION=1.0.2f # http://www.linuxfromscratch.org/blfs/view/svn/postlfs/openssl.html
 NGINX_RTMP_VERSION=1.1.7 # https://github.com/arut/nginx-rtmp-module/releases
 
