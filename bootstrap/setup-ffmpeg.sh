54ef34684fb33c25cf842813d29ca1a30cefe0e9
e7d <mail@e7d.io>
2016-02-16 18:16:25 +0100

    updated ffmpeg version to 2.8.6 using backport repository of deb multimedia

--- bootstrap/setup-ffmpeg.sh
+++ bootstrap/setup-ffmpeg.sh
@@ -12,6 +12,7 @@ try
 	wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2015.6.1_all.deb
 	dpkg -i deb-multimedia-keyring_2015.6.1_all.deb
 	echo "deb http://www.deb-multimedia.org jessie main non-free" >/etc/apt/sources.list.d/ffmpeg.list
+	echo "deb http://www.deb-multimedia.org jessie-backports main" >>/etc/apt/sources.list.d/ffmpeg.list
 	apt-get -y -q update
 
 	echo "Install package"
